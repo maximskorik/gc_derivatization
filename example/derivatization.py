aa0c61c92a178c4fd7881fc0237ea68a7cbd5fa4
wverastegui <wudmir@gmail.com>
2022-07-13 16:42:33 +0200

    Refactoring

--- example/derivatization.py
+++ example/derivatization.py
@@ -1,3 +1,4 @@
+import argparse
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from concurrent.futures import ProcessPoolExecutor
@@ -5,24 +6,20 @@ from gc_meox_src import is_derivatized, remove_derivatization_groups, add_deriva
 import multiprocessing
 import random
 import py3Dmol
-import time
 
-random.seed(42)
-cpus = multiprocessing.cpu_count()
-print('# cpus (including HT, typically): ', cpus)
-cpus //= 2
+# define number of cpus to run 
+def num_cpu():
+    random.seed(42)
+    cpus = multiprocessing.cpu_count()
+#    print('# cpus (including HT, typically): ', cpus)
+    cpus //= 2
 
-#st = time.process_time()
-#et = time.process_time()
-#res = et - st
-#print('CPU Execution time:', res, 'seconds')
 
 # 3D rendering
 def draw3d(m,dimensions=(500,300),p=None):
     AllChem.EmbedMultipleConfs(m, clearConfs=True, numConfs=50)
     opt = AllChem.MMFFOptimizeMoleculeConfs(m)
     conf = min(range(len(opt)),key = lambda x: opt[x][1] if opt[x][0] == 0 else float("inf") )
-    
     mb = Chem.MolToMolBlock(m,confId=conf)
     if p is None:
         p = py3Dmol.view(width=dimensions[0],height=dimensions[1])
@@ -34,73 +31,49 @@ def draw3d(m,dimensions=(500,300),p=None):
     return p.show()
 
 
-# TESTING imported modules
-# is_derivatized
-for s in ['CCC(=NOC)C', 'CCC=NOC', 'C=NOC', 'CSi(C)(C)C']:
-    print(s,is_derivatized(smiles='CCC(=NOC)C'))
-
-# remove_derivatization 
-remove_derivatization_groups(smiles='CCC(=NOC)C')
-
-m=Chem.MolFromSmiles('CCC=NOC')
-remove_derivatization_groups(m)
-draw3d(m)
-remove_derivatization_groups(smiles='C[Si](C)(C)OCCCO[Si](C)(C)C')
-
-m=remove_derivatization_groups(smiles='CON=CC(O)C=NOC')
-
-# add_derivatization
-add_derivatization_groups(m)
-
-
 # reading input file
-smi_file = 'NIST_195_200.txt'
-with open(smi_file) as f:
-    mols = list(filter(lambda p: p[1], [ (smi.rstrip(), Chem.MolFromSmiles(smi)) for smi in f ]))
-
-
-#Essential statistics
-SiMe1=Chem.MolFromSmarts('[Si][CH3]')
-SiMe2=Chem.MolFromSmarts('[Si]([CH3])[CH3]')
-SiMe3=Chem.MolFromSmarts('[Si]([CH3])([CH3])[CH3]')
-ONSSi=Chem.MolFromSmarts('[O,N,S][Si]([CH3])([CH3])[CH3]')
-
-print('# total',len(mols))
-with_sime1 = list(filter(lambda m: m[1].HasSubstructMatch(SiMe1),mols))
-print("# with SiMe:", len(with_sime1))
-with_sime2 = list(filter(lambda m: m[1].HasSubstructMatch(SiMe2),mols))
-print("# with SiMe2:", len(with_sime2))
-with_sime3 = list(filter(lambda m: m[1].HasSubstructMatch(SiMe3),mols))
-print("# with SiMe3:", len(with_sime3))
-with_onssi = list(filter(lambda m: m[1].HasSubstructMatch(ONSSi),mols))
-print("# with ONSSi:", len(with_onssi))
-
-MeOX=Chem.MolFromSmarts('CN(C)C(=O)O[Si](C)(C)C')
-with_meox = list(filter(lambda m: m[1].HasSubstructMatch(MeOX),mols))
-print("# with MeOX:", len(with_meox))
+def read_input_file(filename):
+    smi_file = filename
+    with open(smi_file) as f:
+        mols = list(filter(lambda p: p[1], [ (smi.rstrip(), Chem.MolFromSmiles(smi)) for smi in f ]))
+        return mols
 
 
 # Atcual applcation of the code
-
 def process_one_mol(mol):
     return (
         mol[0],
         Chem.MolToSmiles(remove_derivatization_groups(mol[1])),
         { Chem.MolToSmiles(add_derivatization_groups(mol[1])) for _ in range(42) }
         )
-        
-with ProcessPoolExecutor(max_workers=cpus) as executor:
-    out = executor.map(process_one_mol,mols)
+
+
+def process_pool_exe(filename):
+    with ProcessPoolExecutor(max_workers=num_cpu()) as executor:
+        out = executor.map(process_one_mol,read_input_file(filename))
+        return out
     
-out = list(out)
 
 #Writing outputs
-with open('derivs_struct.tsv','w') as tsv:
-    tsv.write("orig\tderiv. removed\tderiv. added ...\n")
-    for orig,removed,added in out:
-        tsv.write("\t".join([orig,removed,*added]) + "\n")
+def write_derivs_struct(filename):
+    with open('derivs_struct.tsv','w') as tsv:
+        tsv.write("orig\tderiv. removed\tderiv. added ...\n")
+        for orig,removed,added in process_pool_exe(filename):
+            tsv.write("\t".join([orig,removed,*added]) + "\n")
 
-with open('derivs_flat.txt','w') as flat:
-    for orig,removed,added in out:
-        for one in { orig, removed, *added }:
-            flat.write(one + "\n")
+
+def write_derivs_flat(filename):
+    with open('derivs_flat.txt','w') as flat:
+        for orig,removed,added in process_pool_exe(filename):
+            for one in { orig, removed, *added }:
+                flat.write(one + "\n")
+
+
+listarg = argparse.ArgumentParser()
+listarg.add_argument('--filename', type=str)
+args = listarg.parse_args()
+filename = args.filename
+
+if __name__ == "__main__":
+    write_derivs_struct(filename) 
+    write_derivs_flat(filename)

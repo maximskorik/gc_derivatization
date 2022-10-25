26c793762e65439093ccb90709b83c0474cd1674
maximskorik <maximskoryk@gmail.com>
2022-10-25 18:02:40 +0200

    simplify test since its internals are tested elsewhere

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -82,12 +82,12 @@ def test_writing_tsv_content(data, tmp_path):
 
 @pytest.mark.parametrize("smiles, expected", [
     ("CC(N)=O", {"CC(N)=O", "CC(=O)N([Si](C)(C)C)[Si](C)(C)C", "CC(=O)N[Si](C)(C)C"}),
-    ("OC1=CC=C(O)C=C1", {"OC1=CC=C(O)C=C1", "C[Si](C)(C)OC1=CC=C(O[Si](C)(C)C)C=C1", "C[Si](C)(C)OC1=CC=C(O)C=C1"}),
+    ("C[Si](C)(C)OC1=CC=C(O)C=C1", {"OC1=CC=C(O)C=C1", "C[Si](C)(C)OC1=CC=C(O[Si](C)(C)C)C=C1", "C[Si](C)(C)OC1=CC=C(O)C=C1"}),
     ("CCC(C)=O", {"CCC(C)=O", "CCC(C)=NOC"}),
-    ("CC=O", {"CC=O", "CC=NOC"})
+    ("CC=NOC", {"CC=O", "CC=NOC"})
 ])
-def test_process_one_mol_nonderivatized(smiles, expected):
-    """Test processing one derivatized molecule."""
+def test_process_one_mol(smiles, expected):
+    """Test processing one molecule."""
     mol = (smiles, MolFromSmiles(smiles))
     n = 40
     actual = process_one_mol((mol, n))

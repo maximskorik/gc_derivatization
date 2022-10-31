0f320938640ca4f5ef0fb0e8d9e3de90ab2af15f
maximskorik <maximskoryk@gmail.com>
2022-10-31 19:47:34 +0100

    move data path into a fixture

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,15 @@
-import os
 import pytest
 
 from gc_meox_tms.__main__ import main
 from mock import ANY, patch
+from os.path import exists, join
 from random import sample
+
+
+@pytest.fixture
+def input_data_path(test_dir):
+    """Return the directory of the currently running test script as string type."""
+    return join(test_dir, 'data/aldehydes.txt')
 
 
 @pytest.fixture(params=[['-f'], ['-t'], ['-f', '-t']])
@@ -14,40 +20,40 @@ def output_params(request, tmp_path):
     for flag in request.param:
         if flag == '-f':
             args.append(flag)
-            args.append(flat_path := os.path.join(tmp_path, 'flat.txt'))
+            args.append(flat_path := join(tmp_path, 'flat.txt'))
         elif flag == '-t':
             args.append(flag)
-            args.append(tsv_path := os.path.join(tmp_path, 'tsv.txt'))
+            args.append(tsv_path := join(tmp_path, 'tsv.txt'))
     yield args, flat_path, tsv_path
 
 
-def test_cli_finishes():
+def test_cli_finishes(input_data_path):
     """Test if the main function works."""
-    args = ['data/aldehydes.txt']
+    args = [input_data_path]
     exit_code = main(args)
 
     assert exit_code == 0
 
 
-def test_cli_writes_files(output_params, tmp_path):
+def test_cli_writes_files(input_data_path, output_params, tmp_path):
     args = output_params[0]
-    args.append('data/aldehydes.txt')
+    args.append(input_data_path)
 
     flat_path = output_params[1]
     tsv_path = output_params[2]
     exit_code = main(args)
 
     assert exit_code == 0
-    assert flat_path is None or os.path.exists(flat_path)
-    assert tsv_path is None or os.path.exists(tsv_path)
+    assert flat_path is None or exists(flat_path)
+    assert tsv_path is None or exists(tsv_path)
 
 
 @pytest.mark.parametrize('keep', [True, False])
 @patch('gc_meox_tms.__main__.write_flat')
-def test_keep_flag(mock, tmp_path, keep):
+def test_keep_flag(mock, input_data_path, tmp_path, keep):
     """Test if the main function works with -k flag."""
-    flat_path = os.path.join(tmp_path, 'flat.txt')
-    args = ['data/alcohols.txt', '-f', flat_path]
+    flat_path = join(tmp_path, 'flat.txt')
+    args = [input_data_path, '-f', flat_path]
     if keep:
         args.append('-k')
 
@@ -58,9 +64,9 @@ def test_keep_flag(mock, tmp_path, keep):
 
 @pytest.mark.parametrize('num_workers', sample(range(1, 10), 3))
 @patch('gc_meox_tms.__main__.ProcessPoolExecutor')
-def test_ncpu_flag(mock, num_workers):
+def test_ncpu_flag(mock, input_data_path, num_workers):
     """Test if the main function works with -n flag."""
-    args = ['data/alcohols.txt', '-n', str(num_workers)]
+    args = [input_data_path, '-n', str(num_workers)]
     main(args)
 
     mock.assert_called_with(max_workers=num_workers)
@@ -68,9 +74,9 @@ def test_ncpu_flag(mock, num_workers):
 
 @pytest.mark.parametrize('repeats', sample(range(1, 50), 3))
 @patch('gc_meox_tms.__main__.ProcessPoolExecutor.map')
-def test_repeats_flag(mock, repeats):
+def test_repeats_flag(mock, input_data_path, repeats):
     """Test if the main function works with -r flag."""
-    args = ['data/alcohols.txt', '-r', str(repeats)]
+    args = [input_data_path, '-r', str(repeats)]
     main(args)
 
     for params in mock.call_args[0][1]:
